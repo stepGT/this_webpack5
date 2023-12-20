08a3e34353e5a1828b6cbf8c845e6e09d6f75602
Andrey Zuev <stepgt@mail.ru>
2023-12-20 16:18:05 +1000

    Added & configured webpack-dev-server

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,14 +1,17 @@
 ﻿import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import type { Configuration as DevServerConfiguration } from 'webpack-dev-server';
 
 type Mode = 'production' | 'development';
 
 interface EnvVariables {
   mode: Mode;
+  port: number;
 }
 
 export default (env: EnvVariables) => {
+  const isDev = env.mode === 'development';
   const config: webpack.Configuration = {
     mode: env.mode ?? 'development',
     entry: path.resolve(__dirname, 'src', 'index.ts'),
@@ -33,6 +36,13 @@ export default (env: EnvVariables) => {
     resolve: {
       extensions: ['.tsx', '.ts', '.js'],
     },
+    devtool: isDev ? 'inline-source-map' : false,
+    devServer: isDev
+      ? {
+          port: env.port ?? 3000,
+          open: true,
+        }
+      : undefined,
   };
   return config;
 };
