30253d5adc45f1daac826d7cb9eb158c8e75143a
Andrey Zuev <stepgt@mail.ru>
2024-01-19 09:41:56 +1000

    Added babelLoader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,7 +1,7 @@
 ﻿import { ModuleOptions } from 'webpack';
 import { BuildOptions } from './types/types';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import ReactRefreshTypeScript from 'react-refresh-typescript';
+import { buildBabelLoader } from '../babel/buildBabelLoader';
 
 export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
   const isDev = options.mode === 'development';
@@ -45,39 +45,6 @@ export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
     test: /\.s[ac]ss$/i,
     use: [isDev ? 'style-loader' : MiniCssExtractPlugin.loader, cssLoaderWithModule, 'sass-loader'],
   };
-  const tsLoader = {
-    exclude: /node_modules/,
-    test: /\.tsx?$/,
-    use: [
-      {
-        loader: 'ts-loader',
-        options: {
-          getCustomTransformers: () => ({
-            before: [isDev && ReactRefreshTypeScript()].filter(Boolean),
-          }),
-          transpileOnly: true,
-        },
-      },
-    ],
-  };
-  const babelLoader = {
-    test: /\.tsx?$/,
-    exclude: /node_modules/,
-    use: {
-      loader: 'babel-loader',
-      options: {
-        presets: [
-          '@babel/preset-env',
-          '@babel/preset-typescript',
-          [
-            '@babel/preset-react',
-            {
-              runtime: isDev ? 'automatic' : 'classic',
-            },
-          ],
-        ],
-      },
-    },
-  };
+  const babelLoader = buildBabelLoader(options);
   return [assetLoader, scssLoader, babelLoader, svgrLoader];
 }
